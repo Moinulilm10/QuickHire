0195186330cf4ddcccf1ae835530f3d48a2e88a4
Moinul Islam <moinulilm10@gmail.com>
2026-03-07 13:45:37 +0600

    feat: Add a basic root route and enhance the server startup log message.

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello QuickHire from server");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server is running on port ${PORT}. url: http://localhost:${PORT}`,
+  );
 });
